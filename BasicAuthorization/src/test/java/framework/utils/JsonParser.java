cfea81826366dd8e6a8f21e811f073c92008012e
Orlov, Dmitry2 <d2.orlov@a1qa.com>
2019-12-16 17:18:58 +0300

    fix folder, fix json

--- BasicAuthorization/src/test/java/framework/utils/JsonParser.java
+++ BasicAuthorization/src/test/java/framework/utils/JsonParser.java
@@ -1,25 +1,17 @@
 package framework.utils;
 
-import aquality.selenium.logger.Logger;
+import aquality.selenium.utils.JsonFile;
 import org.json.JSONObject;
-import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Paths;
 
 public class JsonParser {
-    public static String getValue(JSONObject jsonObject, String key) {
-
-        return jsonObject.getString(key);
+    public static Object getValue(JSONObject jsonObject, String key) {
+        return jsonObject.get(key);
     }
 
     public static String getDataFromUsersFile(String key) {
-        String result = "";
-        try {
-            result = new JSONObject(new String(Files.readAllBytes(
-                    Paths.get(PropertiesReader.getValue("PATH_TO_FILE_WITH_USER_DATA"))))).getString(key);
-        } catch (IOException e) {
-            Logger.getInstance().error(e.getMessage());
-        }
-        return result;
+        return new JsonFile(PropertiesReader
+                .getValue("PATH_TO_FILE_WITH_USER_DATA"))
+                .getValue("/" + key)
+                .toString();
     }
 }
--- BasicAuthorization/src/test/java/testpackage/AuthorizationTest.java
+++ BasicAuthorization/src/test/java/testpackage/AuthorizationTest.java
@@ -2,9 +2,10 @@ package testpackage;
 
 import aquality.selenium.browser.BrowserManager;
 import dataprovider.DataProviderClass;
+import framework.utils.PropertiesReader;
 import models.User;
-import org.testng.Assert;
 import org.testng.annotations.Test;
+import org.testng.asserts.SoftAssert;
 import pajeobject.AuthorizationResultPage;
 
 public class AuthorizationTest extends BaseTest {
@@ -12,9 +13,12 @@ public class AuthorizationTest extends BaseTest {
     @Test(dataProvider = "DataForAuthorization", dataProviderClass = DataProviderClass.class)
     public void basicAuthorization(User user) {
         logger.info("Step 1: open login Page, authorization");
-        BrowserManager.getBrowser().goTo(String.format("https://%s:%s@httpbin.org/basic-auth/user/passwd", user.getLogin(), user.getPassword()));
+        BrowserManager.getBrowser().goTo(String.format(PropertiesReader.getValue("ULT_TO_TEST_DATA"), user.getLogin(), user.getPassword()));
         logger.info("check authorization page results");
         AuthorizationResultPage authorizationResultPage = new AuthorizationResultPage();
-        Assert.assertEquals(authorizationResultPage.getValueOfAuthorization("user"), user.getLogin(), "It`s not this user");
+        SoftAssert softAssert = new SoftAssert();
+        softAssert.assertEquals(authorizationResultPage.getValueOfAuthorization("user"), user.getLogin());
+        softAssert.assertTrue((Boolean) authorizationResultPage.getValueOfAuthorization("authenticated"));
+        softAssert.assertAll("It`s not this user");
     }
 }
