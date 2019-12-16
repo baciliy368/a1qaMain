193ae890570d95606a847ff23255f7fa3fd8d0ae
Orlov, Dmitry2 <d2.orlov@a1qa.com>
2019-12-16 17:19:53 +0300

    fix folder, fix json

--- BasicAuthorization/src/test/java/testpackage/AuthorizationTest.java
+++ BasicAuthorization/src/test/java/testpackage/AuthorizationTest.java
@@ -13,7 +13,7 @@ public class AuthorizationTest extends BaseTest {
     @Test(dataProvider = "DataForAuthorization", dataProviderClass = DataProviderClass.class)
     public void basicAuthorization(User user) {
         logger.info("Step 1: open login Page, authorization");
-        BrowserManager.getBrowser().goTo(String.format(PropertiesReader.getValue("ULT_TO_TEST_DATA"), user.getLogin(), user.getPassword()));
+        BrowserManager.getBrowser().goTo(String.format(PropertiesReader.getValue("URL_TO_TEST_DATA"), user.getLogin(), user.getPassword()));
         logger.info("check authorization page results");
         AuthorizationResultPage authorizationResultPage = new AuthorizationResultPage();
         SoftAssert softAssert = new SoftAssert();
